9f1c2a3cba153ca44716f4771d0ffaa613bfefc3
Sharan <jeron256@gmail.com>
2025-05-10 14:02:36 +0530

    minor changes

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -39,7 +39,14 @@ export default function Register() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
 
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    trigger,
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
