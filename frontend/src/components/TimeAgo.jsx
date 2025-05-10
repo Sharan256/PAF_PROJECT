c2fc28e5fb6629ce922b9ffa1987df3be49a4db1
Kaveen-Perera <168567828+Kaveen-Perera@users.noreply.github.com>
2025-05-10 22:46:02 +0530

    comment_timeAgo

--- frontend/src/components/TimeAgo.jsx
+++ frontend/src/components/TimeAgo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { formatDistanceToNow, parse } from "date-fns";
 import { enUS } from "date-fns/locale";
 
+// TimeAgo component takes a date prop and displays a relative time (e.g., "5 minutes ago")
 const TimeAgo = ({ date }) => {
   const parsedDate = parse(date, "EEE MMM dd HH:mm:ss 'IST' yyyy", new Date(), {
     locale: enUS,
