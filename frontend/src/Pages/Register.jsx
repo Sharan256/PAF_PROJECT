a1ee850b6ed4736fa18219c7ddffd911b0e09670
Sharan <jeron256@gmail.com>
2025-05-10 15:06:55 +0530

    Unwanted codes remove 1

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -212,7 +212,6 @@ export default function Register() {
               )}
             </div>
 
-            {/* Phone Number Input */}
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
                 Phone Number
