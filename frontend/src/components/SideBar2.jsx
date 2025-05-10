869512662600a8a32fd4044145219cc0c9aadd1a
Kaveen-Perera <168567828+Kaveen-Perera@users.noreply.github.com>
2025-05-10 22:09:10 +0530

    add comment to sideBar

--- frontend/src/components/SideBar2.jsx
+++ frontend/src/components/SideBar2.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// Component receives `logUser` prop to identify the logged-in user
 const SideBar2 = ({ logUser }) => {
   const [users, setUsers] = useState(null);
   // const [logUser, setLogUser] = useState(null);
