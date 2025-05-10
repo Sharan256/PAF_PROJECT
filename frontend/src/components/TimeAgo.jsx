08a1e89adcaa0dfe86594f271af726b684d72eba
Kaveen-Perera <168567828+Kaveen-Perera@users.noreply.github.com>
2025-05-10 22:48:06 +0530

    Coo

--- frontend/src/components/TimeAgo.jsx
+++ frontend/src/components/TimeAgo.jsx
@@ -4,6 +4,9 @@ import { enUS } from "date-fns/locale";
 
 // TimeAgo component takes a date prop and displays a relative time (e.g., "5 minutes ago")
 const TimeAgo = ({ date }) => {
+
+  // Parsing the input date string to a JavaScript Date object.
+  // The format string must match the input format exactly.
   const parsedDate = parse(date, "EEE MMM dd HH:mm:ss 'IST' yyyy", new Date(), {
     locale: enUS,
   });
