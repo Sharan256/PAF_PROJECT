61d2aa3f14f9c48630ceac05196ac81791e5dfbb
Kaveen-Perera <168567828+Kaveen-Perera@users.noreply.github.com>
2025-05-11 09:18:53 +0530

    sideBar

--- frontend/src/components/MainSideBar.jsx
+++ frontend/src/components/MainSideBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaHome, FaPlus, FaClipboardList, FaUtensils, FaSignOutAlt, FaUsers } from "react-icons/fa";
 
+// Sidebar component accepts 'user' and 'isOpen' as props
 const MainSideBar = ({ user, isOpen }) => {
   const handleLogout = async () => {
     try {
