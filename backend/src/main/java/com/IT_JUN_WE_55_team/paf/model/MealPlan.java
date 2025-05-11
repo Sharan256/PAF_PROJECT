dc4854ce6216c86605ecca0e7922d6428b27b21d
SSivanuja <sivanujasureshwaran@gmail.com>
2025-05-11 07:23:20 +0530

    final issue

--- backend/src/main/java/com/IT_JUN_WE_55_team/paf/model/MealPlan.java
+++ backend/src/main/java/com/IT_JUN_WE_55_team/paf/model/MealPlan.java
@@ -13,6 +13,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MealPlan {
 
-
+    @Id
+    private String mealPlanId;
+    private String userId;
+    private String mealType;
+    private String recipes;
+    private String ingredients;
+    private String cookingInstruction;
+    private String nutritionalInformation;
+    private String portionSizes;
+    private String dietaryPreferences;
+    private String source;
+    private String date;
+    private String username;
+    private String userProfile;
 
 }
--- backend/src/main/java/com/IT_JUN_WE_55_team/paf/repo/MealPlanRepository.java
+++ backend/src/main/java/com/IT_JUN_WE_55_team/paf/repo/MealPlanRepository.java
@@ -5,7 +5,3 @@ import org.springframework.stereotype.Repository;
 
 import com.IT_JUN_WE_55_team.paf.model.MealPlan;
 
-@Repository
-public interface MealPlanRepository extends MongoRepository<MealPlan, String> {
-
-}
